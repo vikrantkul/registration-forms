39c2049a17ddcea6f3e54e9cfa3ed168948acf82
vikrant kulkarni <vikrantkul007@gmail.com>
2020-11-22 12:14:43 +0530

    deployment

--- registrations/settings.py
+++ registrations/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!w02j$nw&v7wi!yv=ji$cslt6m-(am9_66w_3yb&m)aku)hlb('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
